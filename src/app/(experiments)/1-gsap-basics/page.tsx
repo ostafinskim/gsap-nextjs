f551f5aabd26a3d81d2a5d871bed6d3cc0c60905
Miro Ostafinski <ostafinskim@gmail.com>
2025-11-20 16:19:55 +0000

    custom cursor and image sequence

--- src/app/(experiments)/1-gsap-basics/page.tsx
+++ src/app/(experiments)/1-gsap-basics/page.tsx
@@ -1,3 +1,44 @@
-import Page from "./starter";
+"use client";
 
-export default Page;
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import { SplitText } from "gsap/all";
+import { useRef } from "react";
+
+gsap.registerPlugin(SplitText);
+
+export default function Page() {
+  const containerRef = useRef<HTMLDivElement>(null);
+
+  useGSAP(
+    () => {
+      const split = SplitText.create(".title");
+
+      gsap.from(split.chars, {
+        autoAlpha: 0,
+        y: 200,
+        duration: 0.4,
+        stagger: 0.03,
+        ease: "circ.out",
+      });
+    },
+    {
+      scope: containerRef,
+    }
+  );
+
+  return (
+    <div className="bg-blue-300 text-black">
+      <div
+        ref={containerRef}
+        className="bg-blue-300 text-black flex h-screen items-end justify-left overflow-hidden"
+      >
+        <h1 className="title font-black text-[20rem] leading-none pb-[0.1em] text-left">
+          GSAP
+          <br />
+          tweens
+        </h1>
+      </div>
+    </div>
+  );
+}
